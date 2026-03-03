076f777e4af8fe58fa10d808a8094c00a075e3cd
victor mwangi <vampmid03@gmail.com>
2026-03-03 19:45:22 +0300

    Update assembly informational version and cache files for consistency

--- obj/Debug/net9.0/IDMSBackend.AssemblyInfo.cs
+++ obj/Debug/net9.0/IDMSBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IDMSBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+391cf38a6546dd2d7392753f109ad165bdf86ac7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c75fd82e16731a69196193e311a72181acd8f63")]
 [assembly: System.Reflection.AssemblyProductAttribute("IDMSBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IDMSBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
